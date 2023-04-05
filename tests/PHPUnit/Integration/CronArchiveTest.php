0aebbfc26b81ffcd030224ec4b9a0efffaaa55f5
sgiehl <stefan@matomo.org>
2023-04-05 16:00:38 +0200

    use di proxy class

--- tests/PHPUnit/Integration/CronArchiveTest.php
+++ tests/PHPUnit/Integration/CronArchiveTest.php
@@ -932,7 +932,7 @@ LOG;
         Date::$now = strtotime('2020-02-03 04:05:06');
 
         return array(
-            'Piwik\CliMulti' => \DI\create('Piwik\Tests\Framework\Mock\FakeCliMulti')
+            'Piwik\CliMulti' => \Piwik\DI::create('Piwik\Tests\Framework\Mock\FakeCliMulti')
         );
     }
 
